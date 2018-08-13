85f75a991d94500bd5369ff4f9212d77647b4a97
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-13 16:45:23 +0300

    log_in_page/setting up the page

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 13.08.2018
+ * Time: 16:37
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class SecurityController
+ * @package AppBundle\Controller
+ */
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        if ($error) {
+            $this->addFlash('danger', $error->getMessage());
+        }
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/logout")
+     */
+    public function logoutAction()
+    {
+        throw new \RuntimeException("the user should never see this.");
+    }
+}
