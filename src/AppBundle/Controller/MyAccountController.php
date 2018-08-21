3e8297f9a1247fb3ba2a048293ca74a2a82d5732
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-21 09:31:18 +0300

    Setting up my_account page

---
+++ src/AppBundle/Controller/MyAccountController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 21.08.2018
+ * Time: 08:49
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class MyAccountController
+ * @package AppBundle\Controller
+ */
+class MyAccountController extends Controller
+{
+    /**
+     * @Route("/my-account", name="my-account")
+     */
+    public function myAccountAction()
+    {
+        return $this->render('my_account/my-account.html.twig');
+    }
+}
