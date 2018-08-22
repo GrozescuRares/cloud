ecb770b04432f23930456836412344dadb078040
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-22 10:58:20 +0300

    Creating the AddUserTypeForm

--- src/AppBundle/Controller/UserManagementController.php
+++ src/AppBundle/Controller/UserManagementController.php
@@ -8,6 +8,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\AddUserTypeForm;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,10 +24,37 @@ class UserManagementController extends Controller
     /**
      * @Route("/user-management/add-user", name="add-user")
      *
+     * @param Request $request
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function addUserAction()
+    public function addUserAction(Request $request)
     {
-        return $this->render('user_management/add-user.html.twig');
+        $user = new User();
+        $loggedUser = $this->getUser();
+
+        $form = $this->createForm(AddUserTypeForm::class, $user, [
+            'loggedUser' => $loggedUser,
+        ]);
+
+        $form->handleRequest($request);
+
+        if (!($form->isSubmitted() && $form->isValid())) {
+            return $this->render(
+                'user_management/add-user.html.twig',
+                [
+                    'add_user_form' => $form->createView(),
+                ]
+            );
+        }
+
+        $this->addFlash('success', 'Add user form successfully submitted. Thank you !');
+
+        return $this->render(
+            'user_management/add-user.html.twig',
+            [
+                'add_user_form' => $form->createView(),
+            ]
+        );
     }
 }
