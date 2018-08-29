f48c88e7fd4533428f61cc8ebf88f510105f4aa0
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-29 17:01:08 +0300

    Creating HotelManagementControllerTest.

---
+++ tests/AppBundle/Controller/HotelManagementControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 29.08.2018
+ * Time: 16:59
+ */
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class HotelManagementControllerTest
+ * @package Tests\AppBundle\Controller
+ */
+class HotelManagementControllerTest extends WebTestCase
+{
+
+}
