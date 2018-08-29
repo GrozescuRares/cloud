a3b7a6973ab473dda4b0892771174a9a9d9b0360
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-29 09:40:27 +0300

    Creating RoomDto.

---
+++ src/AppBundle/Dto/RoomDto.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 29.08.2018
+ * Time: 09:39
+ */
+
+namespace AppBundle\Dto;
+
+/**
+ * Class RoomDto
+ * @package AppBundle\Dto
+ */
+class RoomDto
+{
+    public $capacity;
+    public $price;
+    public $smoking;
+    public $pet;
+    public $hotel;
+}
