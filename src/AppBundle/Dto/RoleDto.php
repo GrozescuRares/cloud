16e3be2f2277587ac7227c69f12ae687185d6432
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-30 15:16:54 +0300

    Created RoleDto.

---
+++ src/AppBundle/Dto/RoleDto.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 30.08.2018
+ * Time: 15:16
+ */
+
+namespace AppBundle\Dto;
+
+/**
+ * Class RoleDto
+ * @package AppBundle\Dto
+ */
+class RoleDto
+{
+    public $description;
+}
