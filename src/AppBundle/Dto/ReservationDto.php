ad945f6d5d170a0d161392f5a837d8d049bd5e8c
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-08-31 10:07:44 +0300

    Creating ReservationDto.

---
+++ src/AppBundle/Dto/ReservationDto.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: intern
+ * Date: 31.08.2018
+ * Time: 10:06
+ */
+
+namespace AppBundle\Dto;
+
+/**
+ * Class ReservationDto
+ * @package AppBundle\Dto
+ */
+class ReservationDto
+{
+    public $user;
+    public $owner;
+    public $room;
+    public $startDate;
+    public $endDate;
+}
