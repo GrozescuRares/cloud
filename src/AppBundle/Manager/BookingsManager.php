17a600a461aacba53d44a5b39e9f097d97d39460
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2018-09-04 12:21:40 +0300

    Added method addReservation in manager.

--- src/AppBundle/Manager/BookingsManager.php
+++ src/AppBundle/Manager/BookingsManager.php
@@ -8,7 +8,10 @@
 
 namespace AppBundle\Manager;
 
+use AppBundle\Dto\ReservationDto;
+use AppBundle\Entity\User;
 use AppBundle\Service\HotelService;
+use AppBundle\Service\ReservationService;
 use AppBundle\Service\RoomService;
 
 /**
@@ -21,17 +24,21 @@ class BookingsManager
     protected $hotelService;
     /** @var RoomService */
     protected $roomService;
+    /** @var ReservationService */
+    protected $reservationService;
 
     /**
      * BookingsManager constructor.
      *
-     * @param HotelService $hotelService
-     * @param RoomService  $roomService
+     * @param HotelService       $hotelService
+     * @param RoomService        $roomService
+     * @param ReservationService $reservationService
      */
-    public function __construct(HotelService $hotelService, RoomService $roomService)
+    public function __construct(HotelService $hotelService, RoomService $roomService, ReservationService $reservationService)
     {
         $this->hotelService = $hotelService;
         $this->roomService = $roomService;
+        $this->reservationService = $reservationService;
     }
 
     /**
@@ -53,5 +60,15 @@ class BookingsManager
     public function getFreeRooms($hotelId, \DateTime $startDate, \DateTime $endDate)
     {
         return $this->roomService->getAvailableRooms($hotelId, $startDate, $endDate);
+    }
+
+    /**
+     * @param User           $client
+     * @param ReservationDto $reservationDto
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function addReservation(User $client, ReservationDto $reservationDto)
+    {
+        $this->reservationService->addReservation($client, $reservationDto);
     }
 }
