466652960fd6078e4710a2b436d508604be83d63
Grozescu Rares <rares.grozescu@pitechnologies.ro>
2019-01-15 18:01:49 +0200

    Azure deploy requirements.

---
+++ app/data/hotel_management_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO `hotel-management`.roles (roleId, description) VALUES (1, 'ROLE_OWNER');
+INSERT INTO `hotel-management`.roles (roleId, description) VALUES (2, 'ROLE_MANAGER');
+INSERT INTO `hotel-management`.roles (roleId, description) VALUES (3, 'ROLE_EMPLOYEE');
+INSERT INTO `hotel-management`.roles (roleId, description) VALUES (4, 'ROLE_CLIENT');
